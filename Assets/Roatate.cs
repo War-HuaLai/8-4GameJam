2abf2fe2aa6eb15eaf04924c0453da080dbba460
小左QAQ <lz1440918141@126.com>
2023-08-04 17:14:36 +0800

    引导提示

--- Assets/Roatate.cs
+++ Assets/Roatate.cs
@@ -7,6 +7,14 @@ public class Roatate : MonoBehaviour
     // Start is called before the first frame update
     public float rotateSpeed;
     private Transform transform;
+    private SpriteRenderer _sp;
+    [SerializeField]Sprite _face;
+    [SerializeField] Sprite _faceNormal;
+    private void Awake()
+    {
+        _sp = GetComponent<SpriteRenderer>();
+    }
+
     void Start()
     {
         transform = GetComponent<Transform>();
@@ -16,5 +24,21 @@ public class Roatate : MonoBehaviour
     void Update()
     {
         transform.Rotate(-transform.forward,Time.deltaTime*rotateSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            _sp.sprite = _face;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            _sp.sprite = _faceNormal;
+        }
     }
 }
